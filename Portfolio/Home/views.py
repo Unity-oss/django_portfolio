e24d6476ce2b991035c8b8c2ef9c3a1ee21a3d8b
unity <unitybusingye9@gmail.com>
2025-09-10 12:12:55 +0300

    first commit

---
+++ Portfolio/Home/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+def aboutMe(request):
+    return render(request,"home.html")
+
+def education(request):
+    return render(request,"edu.html")
+
+def contact(request):
+    context = {}
+    
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        context = {
+            "thank_you" : f"Thank you {name}, your message has been received!"
+        }
+    return render(request,"contact.html", context)
