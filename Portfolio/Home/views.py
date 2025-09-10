c1f326ed2d52dcc237a4a560d9d980d00ef073e8
unity <unitybusingye9@gmail.com>
2025-09-10 12:26:56 +0300

    changed home.html to index.html

--- Portfolio/Home/views.py
+++ Portfolio/Home/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def aboutMe(request):
-    return render(request,"home.html")
+    return render(request,"index.html")
 
 def education(request):
     return render(request,"edu.html")
 
 def contact(request):
     context = {}
-    
+
     if request.method == "POST":
         name = request.POST.get("name")
         email = request.POST.get("email")
